22c53b8b12ea6c4e90456a70727ff34d697dee33
pioneerz <690859614@qq.com>
2020-04-16 14:03:10 +0800

    Bug:删除test code

--- appnopermission/src/main/java/com/huxq17/floatball/nopermission/MainActivity.java
+++ appnopermission/src/main/java/com/huxq17/floatball/nopermission/MainActivity.java
@@ -38,11 +38,6 @@ public class MainActivity extends Activity {
                 }
             });
         }
-
-        // test 1
-        // test 2
-        // test 3
-        // test 4
     }
 
     @Override
